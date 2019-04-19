82342e1819acc906b435860220620a3ac8d2e0fc
nikeshp <nikeshp@umich.edu>
2019-04-19 17:46:04 -0400

    first implementation of logReader class

--- main.py
+++ main.py
@@ -14,3 +14,24 @@ class logger:
             self.priorityTwo.put(message)
         if priority == 3:
             self.priorityThree.put(message)
+
+class logReader(logger):
+
+    def __init__(self, logger_variables):
+        self.priorityOne = logger_variables.priorityOne
+        self.priorityTwo = logger_variables.priorityTwo
+        self.priorityThree = logger_variables.priorityThree
+
+    def get():
+        if self.priorityOne.empty() == False:
+            message = self.priorityOne.get()
+            print(message)
+            return
+        if self.priorityTwo.empty() == False:
+            message = self.priorityTwo.get()
+            print(message)
+            return
+        if self.priorityThree.empty() == False:
+            message = self.priorityThree.get()
+            print(message)
+            return
